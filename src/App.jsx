de76acae8b93716fc27214588edbd84b6764fd70
William C <wiliamcalais89@gmail.com>
2026-03-28 22:17:03 -0300

    Adicionando filtro por categoria

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
 import { useState } from 'react'
 import './App.css'
 import Card from './components/Card.jsx'
-import { ferramentas } from './data/data.js'
+import { itensMinecraft } from './data/data.js'
 
 
 function App() {
+    const [categoria, setCategoria]=useState("Todas")
+    const itensFiltrados=categoria==="Todas"?itensMinecraft:itensMinecraft.filter((item)=>{
+        return item.categoria===categoria
+    })
+    
   return (
     <>
-       {ferramentas.map((f, index)=>{
-           return <Card key={index} nome={f.nome} dano={f.dano} descricao={f.descricao}/>
-       })}
+        <h2>Itens do Minecraft!!</h2>
+        <select value={categoria} onChange={(e)=>setCategoria(e.target.value)}>
+            <option value="Todas">Todas</option>
+            <option value="ferramenta">ferramenta</option>
+            <option value="alimento">alimento</option>
+            <option value="bloco">bloco</option>
+        </select>
+        
+        <div>
+             {itensFiltrados.map((i)=>{
+                 return <Card key={i.id} nome={i.nome} dano={i.dano} descricao={i.descricao} categoria={i.categoria} cura={i.cura}/>
+             })}
+       </div>
     </>
   )
 }
