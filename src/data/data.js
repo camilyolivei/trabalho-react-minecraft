bf2be7cf30d2ce3a39b42bb671ed0cc116f0dab1
Victor-Machado <victorhugosmachado7@gmail.com>
2026-03-31 17:48:25 -0300

    Add linkagem da imagem

--- src/data/data.js
+++ src/data/data.js
@@ -5,70 +5,81 @@ export const itensMinecraft = [
     nome: "Picareta de Diamante",
     categoria: "ferramenta",
     dano: 5,
-    descricao: "Uma das melhores ferramentas, usada para minerar blocos resistentes como obsidiana."
+    descricao: "Uma das melhores ferramentas, usada para minerar blocos resistentes como obsidiana.",
+    imagem: picaretaDima
   },
   {
     id: 2,
     nome: "Espada de Ferro",
     categoria: "ferramenta",
     dano: 6,
-    descricao: "Arma equilibrada e durável, ótima para combate geral."
+    descricao: "Arma equilibrada e durável, ótima para combate geral.",
+    imagem: espadaFerro
   },
   {
     id: 3,
     nome: "Machado de Pedra",
     categoria: "ferramenta",
     dano: 4,
-    descricao: "Ferramenta simples para cortar madeira, com dano moderado."
+    descricao: "Ferramenta simples para cortar madeira, com dano moderado.",
+    imagem: machadoPedra
   },
   {
     id: 4,
     nome: "Pá de Ouro",
     categoria: "ferramenta",
     dano: 2,
-    descricao: "Extremamente rápida, porém com pouca durabilidade."
+    descricao: "Extremamente rápida, porém com pouca durabilidade.",
+    imagem: paOuro
   },
   {
     id: 5,
     nome: "Enxada de Madeira",
     categoria: "ferramenta",
     dano: 1,
-    descricao: "Ferramenta básica para preparar terra para cultivo."
+    descricao: "Ferramenta básica para preparar terra para cultivo.",
+    imagem: enxadaMadeira
   },
   {
     id: 6,
     nome: "Picareta de Ferro",
     categoria: "ferramenta",
     dano: 4,
-    descricao: "Ferramenta muito utilizada, eficiente para mineração avançada."
+    descricao: "Ferramenta muito utilizada, eficiente para mineração avançada.",
+    imagem: picaretaFerro
   },
   {
     id: 7,
     nome: "Espada de Diamante",
     categoria: "ferramenta",
     dano: 7,
-    descricao: "Uma das armas mais poderosas do jogo sem encantamentos."
+    descricao: "Uma das armas mais poderosas do jogo sem encantamentos.",
+    imagem: espadaDima
   },
   {
     id: 8,
     nome: "Machado de Diamante",
     categoria: "ferramenta",
     dano: 6,
-    descricao: "Ferramenta extremamente eficiente para cortar madeira e causa grande dano."
+    descricao: "Ferramenta extremamente eficiente para cortar madeira e causa grande dano.",
+    imagem: machadoDima
   },
   {
     id: 9,
     nome: "Pá de Ferro",
     categoria: "ferramenta",
     dano: 2,
-    descricao: "Boa para remover terra, areia ou cascalho rapidamente."
+    descricao: "Boa para remover terra, areia ou cascalho rapidamente.",
+    imagem: paFerro
+    
   },
   {
     id: 10,
     nome: "Tesoura",
     categoria: "ferramenta",
     dano: 1,
-    descricao: "Usada para coletar lã, folhas e outros blocos sem destruí-los."
+    descricao: "Usada para coletar lã, folhas e outros blocos sem destruí-los.",
+    imagem: tesoura
   },
 
   // -------------------- ALIMENTOS --------------------
@@ -77,42 +88,48 @@ export const itensMinecraft = [
     nome: "Maçã",
     categoria: "alimento",
     cura: 4,
-    descricao: "Alimento simples que recupera um pouco da fome."
+    descricao: "Alimento simples que recupera um pouco da fome.",
+    imagem: maca
   },
   {
     id: 12,
     nome: "Pão",
     categoria: "alimento",
     cura: 5,
-    descricao: "Comida básica feita com trigo, fácil de produzir."
+    descricao: "Comida básica feita com trigo, fácil de produzir.",
+    imagem: pao
   },
   {
     id: 13,
     nome: "Carne Assada",
     categoria: "alimento",
     cura: 8,
-    descricao: "Um dos melhores alimentos comuns, recupera muita fome."
+    descricao: "Um dos melhores alimentos comuns, recupera muita fome.",
+    imagem: carne
   },
   {
     id: 14,
     nome: "Cenoura",
     categoria: "alimento",
     cura: 3,
-    descricao: "Alimento simples muito usado para preparar cenoura dourada."
+    descricao: "Alimento simples muito usado para preparar cenoura dourada.",
+    imagem: cenoura
   },
   {
     id: 15,
     nome: "Batata Assada",
     categoria: "alimento",
     cura: 5,
-    descricao: "Melhora bastante a fome e é fácil de cultivar."
+    descricao: "Melhora bastante a fome e é fácil de cultivar.",
+    imagem: batataAssada
   },
   {
     id: 16,
     nome: "Maçã Dourada",
     categoria: "alimento",
     cura: 4,
-    descricao: "Comida especial que concede regeneração e absorção"
+    descricao: "Comida especial que concede regeneração e absorção",
+    imagem: macaDourada
 },
  
 // --- Blocos ---
@@ -120,61 +137,71 @@ export const itensMinecraft = [
   id: 21,
   nome: "Pedra",
   categoria: "bloco",
-  descricao: "Um dos blocos mais comuns; ao minerar vira pedregulho."
+  descricao: "Um dos blocos mais comuns; ao minerar vira pedregulho.",
+    imagem: pedra
 },
 {
   id: 22,
   nome: "Pedregulho",
   categoria: "bloco",
-  descricao: "Obtido ao minerar pedra; usado para ferramentas e construções."
+  descricao: "Obtido ao minerar pedra; usado para ferramentas e construções.",
+    imagem: pedregulho
 },
 {
   id: 23,
   nome: "Terra",
   categoria: "bloco",
-  descricao: "Bloco básico encontrado na superfície, usado para agricultura."
+  descricao: "Bloco básico encontrado na superfície, usado para agricultura.",
+    imagem: terra
 },
 {
   id: 24,
   nome: "Grama",
   categoria: "bloco",
-  descricao: "Variação da terra coberta; espalha-se para blocos adjacentes."
+  descricao: "Variação da terra coberta; espalha-se para blocos adjacentes.",
+    imagem: grama
 },
 {
   id: 25,
   nome: "Areia",
   categoria: "bloco",
-  descricao: "Afeta pela gravidade; usada para vidro e concreto."
+  descricao: "Afeta pela gravidade; usada para vidro e concreto.",
+    imagem: areia
 },
 {
   id: 26,
   nome: "Areia Vermelha",
   categoria: "bloco",
-  descricao: "Variantes encontrada em biomas de mesa; usada como areia comum."
+  descricao: "Variantes encontrada em biomas de mesa; usada como areia comum.",
+    imagem: areiaVermelha
 },
 {
   id: 27,
   nome: "Cascalho",
   categoria: "bloco",
-  descricao: "Bloco influenciado por gravidade; pode dropar pederneira."
+  descricao: "Bloco influenciado por gravidade; pode dropar pederneira.",
+    imagem: cascalho
 },
 {
   id: 28,
   nome: "Madeira de Carvalho",
   categoria: "bloco",
-  descricao: "Madeira base usada em ferramentas e construções."
+  descricao: "Madeira base usada em ferramentas e construções.",
+  imagem: madreiraCarvalho
 },
 {
   id: 29,
   nome: "Madeira de Bétula",
   categoria: "bloco",
-  descricao: "Madeira clara usada em decorações."
+  descricao: "Madeira clara usada em decorações.",
+  imagem: madeiraBetula
 },
 {
   id: 30,
   nome: "Madeira de Pinheiro",
   categoria: "bloco",
-  descricao: "Madeira escura encontrada em florestas de pinheiros."
+  descricao: "Madeira escura encontrada em florestas de pinheiros.",
+  imagem: madeiraPinheiro
 },
 
 // 31 a 40
@@ -182,61 +209,71 @@ export const itensMinecraft = [
   id: 31,
   nome: "Tábua de Carvalho",
   categoria: "bloco",
-  descricao: "Material versátil para construções e crafting."
+  descricao: "Material versátil para construções e crafting.",
+  imagem: tabuaCarvalho
 },
 {
   id: 32,
   nome: "Carvão Mineral",
   categoria: "bloco",
-  descricao: "Bloco feito para armazenamento de carvão."
+  descricao: "Bloco feito para armazenamento de carvão.",
+  imagem: carvao
 },
 {
   id: 33,
   nome: "Ferro",
   categoria: "bloco",
-  descricao: "Compacta lingotes de ferro; decorativo."
+  descricao: "Compacta lingotes de ferro; decorativo.",
+  imagem: ferro
 },
 {
   id: 34,
   nome: "Ouro",
   categoria: "bloco",
-  descricao: "Bloco decorativo e forma de armazenamento de ouro."
+  descricao: "Bloco decorativo e forma de armazenamento de ouro.",
+  imagem: ouro
 },
 {
   id: 35,
   nome: "Redstone",
   categoria: "bloco",
-  descricao: "Usado para energizar circuitos de redstone."
+  descricao: "Usado para energizar circuitos de redstone.",
+  imagem: redstone
 },
 {
   id: 36,
   nome: "Lápis Lazúli",
   categoria: "bloco",
-  descricao: "Armazena lápis-lazúli; bloco decorativo."
+  descricao: "Armazena lápis-lazúli; bloco decorativo.",
+  imagem: lapisLazuli
 },
 {
   id: 37,
   nome: "Bloco de Diamante",
   categoria: "bloco",
-  descricao: "Armazena diamantes; usado como luxo."
+  descricao: "Armazena diamantes; usado como luxo.",
+  imagem: blocoDima
 },
 {
   id: 38,
   nome: "Bloco de Esmeralda",
   categoria: "bloco",
-  descricao: "Usado para armazenar esmeraldas; estético."
+  descricao: "Usado para armazenar esmeraldas; estético.",
+  imagem: blocoEsmeralda
 },
 {
   id: 39,
   nome: "Vidro",
   categoria: "bloco",
-  descricao: "Bloco transparente criado a partir de areia."
+  descricao: "Bloco transparente criado a partir de areia.",
+  imagem: vidro
 },
 {
   id: 40,
   nome: "Obsidiana",
   categoria: "bloco",
-  descricao: "Extremamente resistente; usada para portais do Nether."
+  descricao: "Extremamente resistente; usada para portais do Nether.",
+  imagem: obsisiana
 },
 
 // 41 a 50
@@ -244,67 +281,78 @@ export const itensMinecraft = [
   id: 41,
   nome: "Tijolos de Pedra",
   categoria: "bloco",
-  descricao: "Material clássico usado em construções robustas."
+  descricao: "Material clássico usado em construções robustas.",
+  imagem: tijolosPedra
 },
 {
   id: 42,
   nome: "Tijolos",
   categoria: "bloco",
-  descricao: "Criados com argila; usados em casas tradicionais."
+  descricao: "Criados com argila; usados em casas tradicionais.",
+  imagem: tijolos
 },
 {
   id: 43,
   nome: "Netherrack",
   categoria: "bloco",
-  descricao: "Encontrado no Nether; queima eternamente."
+  descricao: "Encontrado no Nether; queima eternamente.",
+  imagem: netherrack
 },
 {
   id: 44,
   nome: "Quartzito do Nether",
   categoria: "bloco",
-  descricao: "Bloco branco decorativo usado em construções modernas."
+  descricao: "Bloco branco decorativo usado em construções modernas.",
+  imagem: quartzito
 },
 {
   id: 45,
   nome: "End Stone",
   categoria: "bloco",
-  descricao: "Bloco amarelo encontrado no End; resistente."
+  descricao: "Bloco amarelo encontrado no End; resistente.",
+  imagem: endStone
 },
 {
   id: 46,
   nome: "Concreto Branco",
   categoria: "bloco",
-  descricao: "Bloco sólido usado em construções modernas."
+  descricao: "Bloco sólido usado em construções modernas.",
+  imagem: concretoBranco
 },
 {
   id: 47,
   nome: "Argila",
   categoria: "bloco",
-  descricao: "Encontrada em rios; usada para tijolos."
+  descricao: "Encontrada em rios; usada para tijolos.",
+  imagem: argila
 },
 {
   id: 48,
   nome: "Basalto",
   categoria: "bloco",
-  descricao: "Bloco liso e escuro encontrado no Nether."
+  descricao: "Bloco liso e escuro encontrado no Nether.",
+  imagem: basalto
 },
 {
   id: 49,
   nome: "Deepslate",
   categoria: "bloco",
-  descricao: "Variante profunda da pedra, mais resistente."
+  descricao: "Variante profunda da pedra, mais resistente.",
+  imagem: deepslate
 },
 {
   id: 50,
   nome: "Folhagem de Carvalho",
   categoria: "bloco",
-  descricao: "Bloco decorativo presente em árvores de carvalho."
+  descricao: "Bloco decorativo presente em árvores de carvalho.",
+  imagem: folhagem
 },
 {
   id: 51,
   nome: "Arco",
   categoria: "ferramenta",
   dano: "4-9",
-  descricao: "Arma de longo alcance. Utilizado junto com a flecha. Quando mais mirar meior o dano"
+  descricao: "Arma de longo alcance. Utilizado junto com a flecha. Quando mais mirar meior o dano",
+  imagem: arco
 }
 ]
