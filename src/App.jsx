aff28dcd84d30ad5b52b5ca18160eb4f657817cf
Victor-Machado <victorhugosmachado7@gmail.com>
2026-04-10 00:21:40 -0300

    Adiciona um texto e subtexto

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,14 @@ function App() {
 
   return (
     <>
+      <h1 className="text-6xl md:text-7xl font-extrabold text-center mt-8 mb-4 text-[#2e7d32] tracking-widest drop-shadow-[4px_4px_0_#0d0d0d]">
+        Minecraft Itens
+      </h1>
+
+      <h2 className="text-2xl text-center text-[#1b5e20] font-bold mb-6 tracking-wide">
+        Catálogo de Itens
+      </h2>
+
         <input type="text" placeholder="Buscar item..."
         value={textoBusca}
         onChange={(e) => setTextoBusca(e.target.value)}
