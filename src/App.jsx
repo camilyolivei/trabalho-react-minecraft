d0464c08b3e5ee23c5eb57bb12dd3f89ac8b1225
William C <wiliamcalais89@gmail.com>
2026-04-14 16:13:05 -0300

    Compnentizando mais

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import itensMinecraft from "./data/data.js";
 import ListaCards from "./components/cards/ListaCards.jsx";
 import Categoria from "./components/categoria/Categoria.jsx";
+import Header from "./components/Header.jsx";
 import "../src/assets/style.css";
 
 function App() {
@@ -10,22 +11,9 @@ function App() {
 
   return (
     <>
-      <h1 className="text-6xl md:text-7xl font-extrabold text-center mt-8 mb-4 text-[#2e7d32] tracking-widest drop-shadow-[4px_4px_0_#0d0d0d]">
-        Minecraft Itens
-      </h1>
-
-      <h2 className="text-2xl text-center text-[#1b5e20] font-bold mb-6 tracking-wide">
-        Catálogo de Itens
-      </h2>
-
-        <input type="text" placeholder="Buscar item..."
-        value={textoBusca}
-        onChange={(e) => setTextoBusca(e.target.value)}
-        className="block mx-auto w-11/12 max-w-md p-3 rounded-lg mb-6  border border-gray-300 text-black"/>
-
-      <Categoria setItens={setItensFiltrados} textoBusca={textoBusca} />
-
-      <ListaCards itens={itensFiltrados} />
+    <Header textoBusca={textoBusca} setTextoBusca={setTextoBusca} />
+    <Categoria setItens={setItensFiltrados} textoBusca={textoBusca} />
+    <ListaCards itens={itensFiltrados} />
     </>
   );
 }
