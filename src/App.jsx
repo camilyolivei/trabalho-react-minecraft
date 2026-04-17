ef504525ba68ef416ec3cbb3f9cb5aecde5aa7b8
Victor-Machado <victorhugosmachado7@gmail.com>
2026-04-17 20:07:43 -0300

    Componentização do App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import itensMinecraft from "./data/data.js";
 import ListaCards from "./components/cards/ListaCards.jsx";
 import Categoria from "./components/categoria/Categoria.jsx";
 import "../src/assets/style.css";
+import Header from "./components/Header/Header.jsx";
 
 function App() {
   const [textoBusca, setTextoBusca] = React.useState("");
@@ -10,18 +11,7 @@ function App() {
 
   return (
     <>
-      <h1 className="text-6xl md:text-7xl font-extrabold text-center mt-8 mb-4 text-[#2e7d32] tracking-widest drop-shadow-[4px_4px_0_#0d0d0d]">
-        Minecraft Itens
-      </h1>
-
-      <h2 className="text-2xl text-center text-[#1b5e20] font-bold mb-6 tracking-wide">
-        Catálogo de Itens
-      </h2>
-
-        <input type="text" placeholder="Buscar item..."
-        value={textoBusca}
-        onChange={(e) => setTextoBusca(e.target.value)}
-        className="block mx-auto w-11/12 max-w-md p-3 rounded-lg mb-6  border border-gray-300"/>
+      <Header textoBusca={textoBusca} setTextoBusca={setTextoBusca}/>
 
       <Categoria setItens={setItensFiltrados} textoBusca={textoBusca} />
 
