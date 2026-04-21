4b315ae42fbc1cd6c30a6a7efd218a91751cc8fd
camilyolivei <camilyguilhermeoliveira@gmail.com>
2026-04-21 10:50:05 -0300

    deploy: github pages

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,12 @@ import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
 
-
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/trabalho-react-minecraft/', 
+
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
-    tailwindcss()
+    tailwindcss(),
   ],
 })
